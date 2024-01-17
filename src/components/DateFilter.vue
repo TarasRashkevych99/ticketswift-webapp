f508720eee653c1b3d715a8858a37a3a9ca4bd81
Nicola Mores <51528817+NikoMrs@users.noreply.github.com>
2024-01-17 19:58:53 +0100

    Add DateFilter

---
+++ src/components/DateFilter.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+    <v-row justify="space-around">
+      <v-date-picker v-model="selectedDate" :min="currentDate"></v-date-picker>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'DateFilter',
+  data: () => ({
+    selectedDate: null,
+    currentDate: null,
+    offset: null
+  }),
+  computed: {},
+  async mounted() {
+    this.currentDate = new Date()
+    this.offset = this.currentDate.getTimezoneOffset()
+    this.currentDate = new Date(this.currentDate.getTime() - this.offset * 60 * 1000)
+    this.currentDate.toISOString().split('T')[0]
+  },
+  methods: {}
+}
+</script>
+
+<style scoped></style>
