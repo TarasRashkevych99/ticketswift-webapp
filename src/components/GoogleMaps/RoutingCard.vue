8409a968d73bb309c15065ca5c6a0ef5f12940ed
Nicola Mores <51528817+NikoMrs@users.noreply.github.com>
2024-02-12 14:28:08 +0100

    add Google Maps direction Url

--- src/components/GoogleMaps/RoutingCard.vue
+++ src/components/GoogleMaps/RoutingCard.vue
@@ -1,5 +1,8 @@
 <template>
-  <div id="map"></div>
+  <v-card class="mx-auto" width="70%" height="35%" style="margin-top: 2rem; margin-bottom: 1rem">
+    <div style="margin-bottom: 0.1rem">APRI SU <a :href="`${googleMapsURL}`"> GOOGLE MAPS </a></div>
+    <div id="map"></div>
+  </v-card>
 </template>
 
 <script>
@@ -7,8 +10,7 @@ export default {
   name: 'RoutingCard',
   props: ['lat', 'lon'],
   data: () => ({
-    currentLat: null,
-    currentLon: null
+    googleMapsURL: ''
   }),
   computed: {},
   async mounted() {
@@ -19,6 +21,7 @@ export default {
   methods: {
     /* eslint-disable no-undef */
     initMap() {
+      this.setUpUrl()
       const directionsService = new google.maps.DirectionsService()
       const directionsRenderer = new google.maps.DirectionsRenderer()
       const map = new google.maps.Map(document.getElementById('map'), {
@@ -46,6 +49,18 @@ export default {
           directionsRenderer.setDirections(response)
         })
         .catch((e) => window.alert('Directions request failed due to ' + e))
+    },
+    setUpUrl() {
+      this.googleMapsURL =
+        'https://www.google.com/maps/dir/?api=1&origin=' +
+        sessionStorage.getItem('latitude') +
+        ',' +
+        sessionStorage.getItem('longitude') +
+        '&destination=' +
+        this.lat +
+        ',' +
+        this.lon +
+        '&travelmode=driving'
     }
   }
 }
@@ -53,11 +68,6 @@ export default {
 
 <style scoped>
 #map {
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 100px;
-  padding-bottom: 10px;
+  height: 100%;
 }
 </style>
